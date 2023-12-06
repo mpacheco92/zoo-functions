0f8d58a3fd8a191ccee1aab4afb47f95462f12e1
Mylher Pacheco <mylherpacheco90@gmail.com>
2023-12-06 19:11:02 -0300

    1. Implemente a função getSpeciesByIds.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
+// const {
+//   lionId, ottersId, elephantsId, snakesId, frogsId, bearsId, tigersId, stephanieId, olaId, burlId,
+// } = require('../data/zoo_ids');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) return [];
+  return data.species.filter((specie) => ids.includes(specie.id));
 };
+
+getSpeciesByIds();
 
 module.exports = getSpeciesByIds;
