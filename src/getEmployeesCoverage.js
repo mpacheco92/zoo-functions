d4c88a12d718b898dbd82ad9aea0e0bf6372bba0
Mylher Pacheco <mylherpacheco90@gmail.com>
2023-12-08 17:07:49 -0300

    11. Implemente a função getEmployeesCoverage.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const foundEmployee = (obj) => data.employees.find((employee) =>
+  employee.id === obj.id || employee.firstName === obj.name || employee.lastName === obj.name);
+
+const returnArrayBySpecie = (employee, specieProperty) => data.species
+  .reduce((arr, specie) => {
+    if (employee.responsibleFor.includes(specie.id)) {
+      arr.push(specie[specieProperty]);
+    }
+    return arr;
+  }, []);
+
+const createObj = () => (
+  data.employees.map((employee) => ({
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: returnArrayBySpecie(employee, 'name'),
+    locations: returnArrayBySpecie(employee, 'location'),
+  })));
+
+const getEmployeesCoverage = (obj) => {
+  if (!obj) return createObj();
+  const employee = foundEmployee(obj);
+  if (!employee) {
+    throw new Error('Informações inválidas');
+  }
+
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: returnArrayBySpecie(employee, 'name'),
+    locations: returnArrayBySpecie(employee, 'location'),
+  };
 };
 
 module.exports = getEmployeesCoverage;
