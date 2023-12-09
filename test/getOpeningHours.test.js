132f7df692ad8c8ea0e25df71a25dd4cbfdfc165
Mylher Pacheco <mylherpacheco90@gmail.com>
2023-12-08 21:33:52 -0300

    corrindo LINT requisito 13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -28,7 +28,7 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Thu e 09:00-AM deve lançar uma exceção com a mensagem: "The day must be valid. Example: Monday"', () => {
     expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow('The day must be valid. Example: Monday');
   });
-  it('Para os argumentos Friday e 09:00-ZM deve lançar uma exceção com a mensagem: "The abbreviation must be \"AM\" or \"PM\"', () => {
+  it('Para os argumentos Friday e 09:00-ZM deve lançar uma exceção com a mensagem: "The abbreviation must be "AM" or "PM"', () => {
     expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
   });
 
