fab1f3fc37b8f4d67cf4b27b57372f68b03041fd
Mylher Pacheco <mylherpacheco90@gmail.com>
2023-12-13 16:19:38 -0300

    14. Implemente a função getAnimalMap.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
+const addResidents = (options, specie) => {
+  const residents = [];
+  specie.residents.forEach((resident) => {
+    if (!options.sex || options.sex === resident.sex) {
+      residents.push(resident.name);
+    }
+  });
+
+  if (options.sorted === true) {
+    residents.sort((a, b) => a.localeCompare(b));
+  }
+  return residents;
 };
+
+const getAnimalMap = (options) => {
+  const objLocations = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+
+  data.species.forEach((specie) => {
+    if(!options || !options.includeNames || options.includeNames === false) {
+      objLocations[specie.location].push(specie.name);
+    } else {
+      const specieObj = {};
+      specieObj[specie.name] = addResidents(options, specie);
+      objLocations[specie.location].push(specieObj);
+    }
+  });
+  return objLocations;
+};
+
+
 
 module.exports = getAnimalMap;
