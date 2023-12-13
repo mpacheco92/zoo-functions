eee6a0b0ffdbd463e0f8e6a415c76df33a3c5b43
Mylher Pacheco <mylherpacheco90@gmail.com>
2023-12-13 17:30:12 -0300

    zoo_ids

---
+++ data/zoo_ids.js
@@ -0,0 +1,24 @@
+const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
+const ottersId = '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae';
+const elephantsId = 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5';
+const snakesId = '78460a91-f4da-4dea-a469-86fd2b8ccc84';
+const frogsId = '89be95b3-47e4-4c5b-b687-1fabf2afa274';
+const bearsId = 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d';
+const tigersId = 'e8481c1d-42ea-4610-8e11-1752cfc05a46';
+
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
+module.exports = {
+  lionId,
+  ottersId,
+  elephantsId,
+  snakesId,
+  frogsId,
+  bearsId,
+  tigersId,
+  stephanieId,
+  olaId,
+  burlId,
+};
